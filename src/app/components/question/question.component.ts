5a9bbcd54ba5e0b5e5bf174ac8056c3e93af9c00
Driss_D <gtbdriss@gmail.com>
2023-09-24 14:08:52 +0100

    Done: Progress bar styling, counter interval, reset quiz - startCounter(), stopCounter(), resetCounter(), resetQuiz(), getProgressPercentage();

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -9,19 +9,24 @@ import { QuestionService } from '../../service/question.service';
   styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit{
-  public username!: string;
-  public questionList: any = [];
-  public currentQuestion:number = 1;
-  public points: number = 0;
-  counter=60;
+  public username! : string;
+  public questionList : any = [];
+  public currentQuestion : number = 1;
+  public points : number = 0;
+  counter = 60;
+  correctQuestion : number = 0;
+  incorrectQuestion : number = 0;
+  interval$ : any;
+  progress : string = '0';
 
 
   constructor(private questionService: QuestionService) { }
 
   ngOnInit(): void {
     this.username = localStorage.getItem('username')!;
-    this.getAllQuestions()
-  }
+    this.getAllQuestions();
+    this.startCounter();
+  };
 
   getAllQuestions() {
     this.questionService.getQuestionJson()
@@ -29,25 +34,75 @@ export class QuestionComponent implements OnInit{
         console.log(res.questions)
         this.questionList = res.questions;
       })
-  }
+  };
 
   nextQuestion(){
     this.currentQuestion++;
-  }
+    this.resetCounter();
+    this.getProgressPercentage();
+  };
   previousQuestion() {
     this.currentQuestion--;
-  }
-  resetQuiz(){
+    this.getProgressPercentage();
+  };
 
-  }
   answer(toNextQuestion:number, option:any){
     if (option.correct) {
       this.points += 10;
+      this.correctQuestion++;
+      // this.currentQuestion++;
+      // this.getProgressPercentage();
+
     } else {
       this.points -= 2;
+      this.incorrectQuestion++;
+      // this.currentQuestion++;
+      // this.getProgressPercentage();
     }
 
     this.currentQuestion = toNextQuestion+1;
-  }
+    this.resetCounter();
+    this.getProgressPercentage();
+  };
+  startCounter(){
+    this.interval$ = interval(1000).subscribe(val=>{
+      this.counter--; //each 1sec the counter will mines by 1.
+      if(this.counter == 0 ){ //once the counter equals to 0
+        this.currentQuestion++; // -pass to the next question
+        this.counter=60; // -reset the counter to 60sec
+        this.points-=2; // -take the answer as wrong;
+      }
+    });
+    setInterval(() => {
+      this.interval$.unsubscribe();
+    }, 600000);
+  };
+  stopCounter(){
+    this.interval$.unsubscribe();
+    this.counter = 0;
+  };
+  resetCounter(){
+    this.stopCounter();
+    this.counter = 60;
+    this.startCounter();
+  };
+  resetQuiz(){
+    this.resetCounter();
+    this.getAllQuestions();
+    this.points = 0;
+    this.currentQuestion = 0;
+    this.getProgressPercentage();
+  };
+  getProgressPercentage(){
+    this.progress = ((this.currentQuestion/this.questionList.length)*100).toString();
+    return this.progress;
+  };
 
 }
+
+
+/*
+RXJS
+var$ OBSARVABLE
+
+*/
