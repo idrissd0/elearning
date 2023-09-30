ab0d5e658c9f0a79863e56564e0ba8f80b66d330
Driss_D <gtbdriss@gmail.com>
2023-09-30 12:50:22 +0100

    fixed structer issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { QuestionComponent } from './components/question/quizQuestions/question.component';
+import { QuestionComponent } from './components/quiz/quizQuestions/question.component';
 
 const routes: Routes = [
   {path:"", redirectTo: 'welcome', pathMatch:"full"},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { QuestionComponent } from './components/question/quizQuestions/question.component';
+import { QuestionComponent } from './components/quiz/quizQuestions/question.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import {ChangeBgDirective} from './directives/change-bg.directive';
-import { QuizWelcomeComponent } from './components/question/quiz-welcome.component';
+import { QuizWelcomeComponent } from './components/quiz/quiz-welcome.component';
 
 @NgModule({
   declarations: [
